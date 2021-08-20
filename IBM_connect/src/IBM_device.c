2380325d3a40a87d44132994062028df9fc8b479
PMeskauskas <paulius134679@gmail.com>
2021-08-20 13:47:01 +0300

    cleaned main() method

--- IBM_connect/src/IBM_device.c
+++ IBM_connect/src/IBM_device.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <memory.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/resource.h>/ 
+#include <sys/resource.h>
 #include <iotp_device.h>
 #include <syslog.h>
 volatile int interrupt = 0;
 
+void cleanup()
+{
+    closelog();
+    exit(1);
+}
+
+void cleanupAll(IoTPDevice *device, IoTPConfig *config)
+{
+    IoTPDevice_destroy(device);
+    IoTPConfig_clear(config);
+    cleanup();
+}
+
 void usage(void) {
     syslog(LOG_ERR, "Usage: \"orgId\" \"typeId\" \"deviceId\" \"authToken\"");
-    exit(1);
+    cleanup();
 }
 
 void sigHandler(int signo) {
@@ -26,71 +38,110 @@ void MQTTTraceCallback (int level, char * message)
         syslog(LOG_INFO, "%s", message ? message:"NULL");
 }
 
+void configCreate(IoTPConfig **config, char *argv[])
+{
+    int rc = 0;
+    rc = IoTPConfig_create(config, NULL);
+    if ( rc != 0 ) {
+        syslog(LOG_ERR, "Failed to initialize configuration: rc=%d", rc);
+        IoTPConfig_clear(*config);
+        cleanup();
+    }
+    IoTPConfig_setProperty(*config, "identity.orgId", argv[1]);
+    IoTPConfig_setProperty(*config, "identity.typeId", argv[2]);
+    IoTPConfig_setProperty(*config, "identity.deviceId", argv[3]);
+    IoTPConfig_setProperty(*config, "auth.token", argv[4]);
+}
+
+void deviceCreate(IoTPDevice **device, IoTPConfig *config)
+{
+    int rc = 0;
+    rc = IoTPDevice_create(device, config);
+    if ( rc != 0 ) {
+        syslog(LOG_ERR, "Failed to configure IoTP device: rc=%d", rc);
+        cleanupAll(*device, config);
+    }
+}
+
+void deviceConnect(IoTPDevice **device, IoTPConfig *config)
+{
+    int rc = 0;
+    rc = IoTPDevice_connect(*device);
+    if ( rc != 0 ) {
+        syslog(LOG_ERR, "Failed to connect to Watson IoT Platform: rc=%d", rc);
+        syslog(LOG_ERR, "Returned error reason: %s\n", IOTPRC_toString(rc));
+        cleanupAll(*device, config);
+    }
+}
+
+void deviceDisconnect(IoTPDevice *device, IoTPConfig *config)
+{
+    int rc = 0;
+    rc = IoTPDevice_disconnect(device);
+    if ( rc != 0 ) {
+        syslog(LOG_ERR, "Failed to disconnect from  Watson IoT Platform: rc=%d", rc);
+        cleanupAll(device, config);
+    }
+}
+
+void calculateMemory(float *memtotal, float *memfree)
+{
+    char buff[256];
+    FILE *fp;
+    fp = fopen("/proc/meminfo", "r");
+    if(fp != NULL){
+    fscanf(fp, "%s", buff);
+    fscanf(fp, "%s", buff);
+    *memtotal = atoi(buff)/1000.0;
+    fscanf(fp, "%s", buff);
+    fscanf(fp, "%s", buff);
+    fscanf(fp, "%s", buff);
+    *memfree = atoi(buff)/1000.0;
+    }
+    else
+    {
+        memtotal = 0;
+        memfree = 0;
+    }
+    fclose(fp);
+}
+
+void deviceSendEvent(IoTPDevice *device)
+{
+    char data[256];
+    int rc = 0;
+    while(!interrupt){
+        float memtotal = 0;
+        float memfree = 0;
+        calculateMemory(&memtotal, &memfree);
+        sprintf(data,"{\"Memory usage\": \"%.2f MB/ %.2f MB\"}", memtotal-memfree, memtotal);
+        rc = IoTPDevice_sendEvent(device,"status", data, "json", QoS0, NULL);
+        syslog(LOG_INFO, "RC from sendEvent(): %d\n", rc);
+        sleep(10);
+        }
+    }
+
 int main(int argc, char *argv[])
 {
-    int useEnv = 0;
-    int testCycle = 0;
-    int rc = 0;
-    struct rusage r_usage;
-    char data[256];
     IoTPConfig *config = NULL;
     IoTPDevice *device = NULL;
     openlog(NULL, LOG_CONS, LOG_USER);
-    syslog(LOG_INFO,"argv[1]: %s argv[2]: %s argv[3]: %s argv[4]: %s",argv[1],argv[2],argv[3],argv[4]);
+    syslog(LOG_INFO,"orgId: %s typeId: %s deviceId: %s authToken: %s",argv[1],argv[2],argv[3],argv[4]);
     
     if ( argc != 5 )
         usage();
     signal(SIGINT, sigHandler);
     signal(SIGTERM, sigHandler);
     
-    rc = IoTPConfig_create(&config, NULL);
-    IoTPConfig_setProperty(config, "identity.orgId", argv[1]);
-    IoTPConfig_setProperty(config, "identity.typeId", argv[2]);
-    IoTPConfig_setProperty(config, "identity.deviceId", argv[3]);
-    IoTPConfig_setProperty(config, "auth.token", argv[4]);
+    configCreate(&config, argv);
     
-    if ( rc != 0 ) {
-        syslog(LOG_ERR, "Failed to initialize configuration: rc=%d", rc);
-        exit(1);
-    }
-    
-    rc = IoTPDevice_create(&device, config);
-    if ( rc != 0 ) {
-        syslog(LOG_ERR, "Failed to configure IoTP device: rc=%d", rc);
-        usage();
-        exit(1);
-    }
+    deviceCreate(&device, config);
+    deviceConnect(&device, config);
 
-    rc = IoTPDevice_setMQTTLogHandler(device, &MQTTTraceCallback);
-    if ( rc != 0 ) {
-        syslog(LOG_ERR, "Failed to set MQTT Trace handler: rc=%d", rc);
-    }
+    deviceSendEvent(device);
 
-    rc = IoTPDevice_connect(device);
-    if ( rc != 0 ) {
-        syslog(LOG_ERR, "Failed to connect to Watson IoT Platform: rc=%d", rc);
-        syslog(LOG_ERR, "Returned error reason: %s\n", IOTPRC_toString(rc));
-        exit(1);
-    }
-    
-    char *commandName = "+";
-    char *format = "+";
-    
-    IoTPDevice_subscribeToCommands(device, commandName, format);
-    while(!interrupt)
-    {
-        getrusage(RUSAGE_SELF,&r_usage);
-        sprintf(data,"{\"Memory usage\": \"%ld Kbs\"}", r_usage.ru_maxrss);
-        rc = IoTPDevice_sendEvent(device,"status", data, "json", QoS0, NULL);
-        syslog(LOG_INFO, "RC from sendEvent(): %d\n", rc);
-        sleep(20);
-    }
-    
-    rc = IoTPDevice_disconnect(device);
-    if ( rc != 0 ) {
-        syslog(LOG_ERR, "Failed to disconnect from  Watson IoT Platform: rc=%d", rc);
-        exit(1);
-    }
+    deviceDisconnect(device, config);
+
     IoTPDevice_destroy(device);
     IoTPConfig_clear(config);
     closelog();
